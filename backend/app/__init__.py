75ff99cbe662dca4c90deb6281ffc387e46e5dc9
Lokesh5631 <lokeshns5631@gmail.com>
2026-01-20 01:00:58 +0530

    Framework update

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,15 @@
+from flask import Flask
+from app.extensions import db
+from app.config import Config
+# from app.models import Community, User
+
 def create_app():
-    from flask import Flask
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    
+    from app.routes.community import community_bp
+    app.register_blueprint(community_bp)
+    # with app.app_context():
+    #     from app import models
     return app
