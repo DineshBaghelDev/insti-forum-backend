3ca24262a1d81a3c2b211eb053c7cd5c098a7a0f
Lokesh5631 <lokeshns5631@gmail.com>
2026-01-25 13:24:30 +0530

    update models

--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -1,9 +1,10 @@
 from app.extensions import db
 
 class Post(db.Model): 
+  __tablename__ = "post"
   id = db.Column(db.Integer, primary_key = True) 
-  user_id= db.Column(db.Integer) 
-  community_id = db.Column(db.Integer) 
-  title = db.Column(db.String(100)) 
+  user_id= db.Column(db.Integer, db.ForeignKey('user.id')) 
+  community_id = db.Column(db.Integer, db.ForeignKey('community.id')) 
+  title = db.Column(db.String(100), nullable=False) 
   content = db.Column(db.String(1000)) 
   created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
